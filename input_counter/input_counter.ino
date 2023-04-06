14a1a6b90bd71b203e8f787919d760ce40df38af
Arif Basharyar <saye.arif@gmail.com>
2023-04-06 09:30:10 +0300

    test another type of button functionality

--- input_counter/input_counter.ino
+++ input_counter/input_counter.ino
@@ -17,29 +17,61 @@ The circuit:
 #include <LiquidCrystal.h>
 
 // Variables
-const int inPin = 2;       // pushbutton connected to digital pin 2
-volatile int counter = 0;  // variable to store the read value
+static int inPin1 = 18;
+static int inPin2 = 17;
+static int inPin3 = 16;
+static int inPin4 = 15;
+// volatile int counter = 0;  // variable to store the read value
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 11, en = 12, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// Functions
-void pulse_interrupt(void) {
-  counter++;
-}
+// // Functions
+// void pulse_interrupt(void) {
+//   counter++;
+// }
 
 void setup() {
   // Initialize input and attachInterrupt
-  pinMode(inPin, INPUT_PULLUP);                                                   // Counting Pin 2 an input
-  attachInterrupt(digitalPinToInterrupt(inPin), pulse_interrupt, RISING);  // Interrupt routine definition
+  // pinMode(inPin, INPUT_PULLUP);                                                   // Counting Pin 2 an input
+  // attachInterrupt(digitalPinToInterrupt(inPin), pulse_interrupt, RISING);  // Interrupt routine definition
   // Initalize the screen
+
+  pinMode(inPin1, INPUT);
+  pinMode(inPin2, INPUT);
+  pinMode(inPin3, INPUT);
+  pinMode(inPin4, INPUT);
+
   lcd.begin(16, 2);
 }
 
 void loop() {
   // Write the counter to screen
-  lcd.setCursor(0, 0);
-  lcd.print(counter);
+  // lcd.setCursor(0, 0);
+  // lcd.print(counter);
+    lcd.setCursor(0, 0);
+    lcd.print("ready");
+
+  if (digitalRead(inPin1) == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("state1");
+  }
+  if (inPin2 == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("state2");
+  }
+  if (inPin3 == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("state3");
+  }
+  if (inPin4 == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("state4");
+  }
 }
+
+// void change_state(){
+
+// }
